75a94d7102f92da7104e549cfb14760a8d60a220
AshishKohad27 <kohadashish27@gmail.com>
2023-01-21 15:22:05 +0530

    cors already present

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use("/register", registerRoute);
 app.use("/calculate", calculateRoute);
 
